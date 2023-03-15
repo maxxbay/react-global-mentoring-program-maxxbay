ea0e3a2dd12fe0090ec906d8dfcc3b9c2d663c2b
Maxxbay <maxxbay@gmail.com>
2023-03-15 12:04:31 +0100

    Task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import Homepage from 'components/HomePage';
+import Counter from 'pages/Task1/Counter';
+import MovieList from 'pages/Task1/MovieList';
+import SearchForm from 'pages/Task1/SearchForm';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Learn React global</h1>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Homepage />
+        <Routes>
+          <Route exact path="/counter" element={<Counter />} />
+          <Route exact path="/search-form" element={<SearchForm />} />
+          <Route exact path="/genre-search" element={<MovieList />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
---
+++ src/components/HomePage.js
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import './HomePage.scss';
+
+const Homepage = () => {
+  return (
+    <div className="home">
+      <h1>React global mentoring program - Task 1</h1>
+      <ul className="style">
+        <li className="style-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="style-item">
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li className="style-item">
+          <Link to="/search-form">Search Form</Link>
+        </li>
+        <li className="style-item">
+          <Link to="/genre-search">Search Film Genre</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Homepage;
