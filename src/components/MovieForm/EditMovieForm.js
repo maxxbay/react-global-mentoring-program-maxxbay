efe2e9cbae8b6a17f8195c250813b04b94c29682
Maxxbay <maxxbay@gmail.com>
2023-05-04 18:09:55 +0200

    Update EditMovieForm.js

--- src/components/MovieForm/EditMovieForm.js
+++ src/components/MovieForm/EditMovieForm.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Dialog from '../Dialog/Dialog';
 import MovieForm from './MovieForm';
 import { API_EDIT_URL } from '../../constants';
+import { editMovieData } from '../../helpers';
 import useFetch from '../../Hooks/useFetch';
 
 const EditMovieForm = () => {
@@ -18,14 +19,24 @@ const EditMovieForm = () => {
     }
   }, [movieId, getData]);
 
-  const onSubmit = async data => {
-    try {
-      await put(movieId, data);
-      navigate('/');
-    } catch (error) {
-      console.error('Error updating movie:', error.response || error);
-    }
+  const [submitted, setSubmitted] = useState(false);
+  const [movieData, setMovieData] = useState({});
+
+  const onSubmit = data => {
+    setMovieData(editMovieData(movieId, data));
+    setSubmitted(true);
   };
+
+  useEffect(() => {
+    if (submitted && movieData) {
+      const updateMovie = async () => {
+        await put(movieId, movieData);
+        setSubmitted(false);
+        navigate('/');
+      };
+      updateMovie();
+    }
+  }, [submitted, movieData, movieId, put, navigate]);
 
   const handleClose = () => {
     navigate('/');
