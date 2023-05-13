d129ccad78ca7f931db95bc3031fc94cf36c9879
Maxxbay <maxxbay@gmail.com>
2023-05-13 18:08:58 +0200

    Worked Search Functionality - YOUUHOUUUU

--- SSR-APP_remix/app/components/SearchForm/SearchForm.jsx
+++ SSR-APP_remix/app/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useContext } from 'react';
+import { useLoaderData, Form } from '@remix-run/react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import SearchContext from '../../routes/SearchContext';
 import Button from '../Button/Button';
 import { Outlet } from 'react-router-dom';
+import { API_URL } from '../../constants';
 
 import styleForm from '~/styles/SearchForm.css';
 
@@ -10,32 +13,25 @@ export function StyleFormLinks() {
   return <link rel="stylesheet" href={styleForm} />;
 }
 
+export async function action({ request }) {
+  let formData = await request.formData();
+  const movieName = formData.get('movieName');
+
+  return movieName;
+}
+
 const SearchForm = ({ initialSearchQuery }) => {
-  const [value, setValue] = useState(initialSearchQuery || '');
-  const { searchQuery, handleSearchQueryChange } = useContext(SearchContext);
-
-  const handleForm = event => {
-    event.preventDefault();
-
-    if (value.trim() === '') {
-      handleSearchQueryChange('');
-    } else {
-      handleSearchQueryChange(value);
-    }
-  };
-
   return (
     <div className="search">
       <h1 className="search-title">Find your movie</h1>
-      <form onSubmit={handleForm}>
+      <Form method="get">
         <input
           type="text"
+          name="query"
           placeholder="What do you want to watch?"
-          value={value}
-          onChange={({ target: { value } }) => setValue(value)}
         />
         <Button type="submit">search</Button>
-      </form>
+      </Form>
       <Outlet />
     </div>
   );
